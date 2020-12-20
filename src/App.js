bd4c453aff2bd811a8b9f21eaef9dc844644d6f9
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-20 18:57:00 +0700

    Navbar with styled component and global styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello react</h1>
-    
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Nav } from './NavBar.elements';
+
+const NavBar = () => {
+    return (
+        <div>
+            <Nav> Test </Nav>
+        </div>
+    )
+}
+
+export default NavBar
