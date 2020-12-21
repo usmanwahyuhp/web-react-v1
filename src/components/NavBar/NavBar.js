162e126147b6107c5d159c6c1d852f9897f1da1a
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-21 17:41:54 +0700

    Adding sign up button

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Nav,
     NavbarContainer,
@@ -7,15 +7,33 @@ import {
     MobileIcon,
     NavMenu,
     NavItem,
-    NavLinks
+    NavLinks,
+    NavBtnLink,
+    NavItemBtn
   } from './NavBar.elements';
 import { FaTimes, FaBars } from 'react-icons/fa';
 import { IconContext } from 'react-icons/lib';
+import { Button } from '../../globalStyles'
 
 const NavBar = () => {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
+
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+          setButton(false);
+        } else {
+          setButton(true);
+        }
+      };
+
+      useEffect(() => {
+        showButton();
+      }, []);
+
+      window.addEventListener('resize', showButton);
 
     return (
         <>
@@ -45,6 +63,19 @@ const NavBar = () => {
                                 Products
                                 </NavLinks>
                             </NavItem>
+                            <NavItemBtn>
+                                {button ? (
+                                <NavBtnLink to='/sign-up'>
+                                    <Button primary>SIGN UP</Button>
+                                </NavBtnLink>
+                                ) : (
+                                <NavBtnLink to='/sign-up'>
+                                    <Button fontBig primary>
+                                    SIGN UP
+                                    </Button>
+                                </NavBtnLink>
+                                )}
+                            </NavItemBtn>
                         </NavMenu>
                     </NavbarContainer>
                 </Nav>
