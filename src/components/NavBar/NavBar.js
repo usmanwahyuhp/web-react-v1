bc3dc4b8ffd32fc4ac8e0d79f66df75119a661fc
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-21 16:40:12 +0700

    Nav menu using links

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,26 +1,45 @@
 import React, { useState } from 'react';
-import { Nav, NavbarContainer, NavIcon, NavLogo, MobileIcon  } from './NavBar.elements';
+import {
+    Nav,
+    NavbarContainer,
+    NavIcon,
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks
+  } from './NavBar.elements';
 import { FaTimes, FaBars } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 
 const NavBar = () => {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
-    
+
     return (
-        <div>
-            <Nav>
-                <NavbarContainer>
-                    <NavLogo to="/">
-                        <NavIcon/>
-                        USMAN
-                    </NavLogo>
-                    <MobileIcon>
-                        {click ? <FaTimes /> : <FaBars />}
-                    </MobileIcon>
-                </NavbarContainer>
-            </Nav>
-        </div>
+        <>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <Nav>
+                    <NavbarContainer>
+                        <NavLogo to="/">
+                            <NavIcon/>
+                            USMAN
+                        </NavLogo>
+                        <MobileIcon onClick={handleClick}>
+                            {click ? <FaTimes /> : <FaBars />}
+                        </MobileIcon>
+                        <NavMenu onClick={handleClick}>
+                            <NavItem>
+                                <NavLinks to='/'>
+                                Home
+                                </NavLinks>
+                            </NavItem>
+                        </NavMenu>
+                    </NavbarContainer>
+                </Nav>
+            </IconContext.Provider>
+        </>
     )
 }
 
