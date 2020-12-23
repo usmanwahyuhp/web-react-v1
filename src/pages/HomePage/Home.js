6b06a6fee89675708ece1e37cfedc36e1feece62
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-23 22:53:18 +0700

    Adding another home object

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { InfoSection } from '../../components';
-import { homeObjOne } from './Data'
+import { homeObjFour, homeObjOne, homeObjThree } from './Data'
 
 const Home = () => {
     return (
         <div>
             <InfoSection {...homeObjOne} />
+            <InfoSection {...homeObjThree} />
+            <InfoSection {...homeObjFour} />
         </div>
     )
 }
