14b6eea01f1932b4f8a1397fe7a4cf817d508610
usmanwahyuhp <usmanwahyuhp@gmail.com>
2020-12-24 17:01:47 +0700

    Footer Links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,18 @@
 import React from 'react'
-import { FooterContainer, FooterSubHeading, FooterSubscription, FooterSubText } from './Footer.elements'
+import { Button } from '../../globalStyles'
+import { 
+    FooterContainer, 
+    FooterSubHeading, 
+    FooterSubscription, 
+    FooterSubText, 
+    Form,
+    FormInput,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink
+} from './Footer.elements'
 
 const Footer = () => {
     return (
@@ -9,7 +22,46 @@ const Footer = () => {
                     Join our exclusive membership to receive the latest news and trends
                     </FooterSubHeading>
                     <FooterSubText>You can unsubscribe at any time.</FooterSubText>
+                    <Form>
+                        <FormInput name='email' type='email' placeholder='Your Email' />
+                        <Button fontBig>Subscribe</Button>
+                    </Form>
                 </FooterSubscription>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to='/sign-up'>How it works</FooterLink>
+                            <FooterLink to='/'>Testimonials</FooterLink>
+                            <FooterLink to='/'>Careers</FooterLink>
+                            <FooterLink to='/'>Investors</FooterLink>
+                            <FooterLink to='/'>Terms of Service</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                            <FooterLink to='/'>Contact</FooterLink>
+                            <FooterLink to='/'>Support</FooterLink>
+                            <FooterLink to='/'>Destinations</FooterLink>
+                            <FooterLink to='/'>Sponsorships</FooterLink>
+                        </FooterLinkItems>
+                        </FooterLinksWrapper>
+                        <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Videos</FooterLinkTitle>
+                            <FooterLink to='/'>Submit Video</FooterLink>
+                            <FooterLink to='/'>Ambassadors</FooterLink>
+                            <FooterLink to='/'>Agency</FooterLink>
+                            <FooterLink to='/'>Influencer</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>Social Media</FooterLinkTitle>
+                            <FooterLink to='/'>Instagram</FooterLink>
+                            <FooterLink to='/'>Facebook</FooterLink>
+                            <FooterLink to='/'>Youtube</FooterLink>
+                            <FooterLink to='/'>Twitter</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
             </FooterContainer>
     )
 }
